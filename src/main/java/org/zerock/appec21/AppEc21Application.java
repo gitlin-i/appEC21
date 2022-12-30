be6faf6f6f5a39ef64c68be0d2d5f72135b4cefe
gitlin-i <70059525+gitlin-i@users.noreply.github.com>
2022-12-30 17:50:21 +0900

    1

---
+++ src/main/java/org/zerock/appec21/AppEc21Application.java
@@ -0,0 +1,13 @@
+package org.zerock.appec21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEc21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEc21Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/appec21/AppEc21ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.appec21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
